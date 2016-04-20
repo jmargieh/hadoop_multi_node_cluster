1c1e171c214791a904356faaa76bb0e1b4d1fac6
Jawad Margieh <J.margieh@gmail.com>
2016-04-20 10:11:12 +0300

    changed config of slave master

--- configure_hadoop_master.sh
+++ configure_hadoop_master.sh
@@ -15,18 +15,18 @@ cd /home/vagrant/hadoop-2.7.2/etc/hadoop/
 #get configuration files
 #OK
 rm -f core-site.xml
-wget https://raw.githubusercontent.com/jmargieh/hadoop_cluster/master/core-site.xml
+wget https://raw.githubusercontent.com/jmargieh/hadoop_single_node_cluster/master/core-site.xml
 # only on master
 rm -f mapred-site.xml
-wget https://raw.githubusercontent.com/jmargieh/hadoop_cluster/master/mapred-site.xml
+wget https://raw.githubusercontent.com/jmargieh/hadoop_single_node_cluster/master/mapred-site.xml
 #OK
 rm -f hdfs-site.xml
-wget https://raw.githubusercontent.com/jmargieh/hadoop_cluster/master/master-hdfs/hdfs-site.xml
+wget https://raw.githubusercontent.com/jmargieh/hadoop_single_node_cluster/master/master-hdfs/hdfs-site.xml
 #OK
 rm -f yarn-site.xml
-wget https://raw.githubusercontent.com/jmargieh/hadoop_cluster/master/yarn-site.xml
+wget https://raw.githubusercontent.com/jmargieh/hadoop_single_node_cluster/master/yarn-site.xml
 #OK
-rm -f slaves
-wget https://raw.githubusercontent.com/jmargieh/hadoop_cluster/master/slaves
+#rm -f slaves
+#wget https://raw.githubusercontent.com/jmargieh/hadoop_cluster/master/slaves
 #OK
-wget https://raw.githubusercontent.com/jmargieh/hadoop_cluster/master/masters
+#wget https://raw.githubusercontent.com/jmargieh/hadoop_cluster/master/masters
--- configure_hadoop_slave.sh
+++ configure_hadoop_slave.sh
@@ -16,10 +16,13 @@ cd /home/vagrant/hadoop-2.7.2/etc/hadoop/
 #get configuration files
 #OK
 rm -f core-site.xml
-wget https://raw.githubusercontent.com/jmargieh/hadoop_cluster/master/core-site.xml
+wget https://raw.githubusercontent.com/jmargieh/hadoop_single_node_cluster/master/core-site.xml
+# only on master
+rm -f mapred-site.xml
+wget https://raw.githubusercontent.com/jmargieh/hadoop_single_node_cluster/master/mapred-site.xml
 #OK
 rm -f hdfs-site.xml
-wget https://raw.githubusercontent.com/jmargieh/hadoop_cluster/master/slave-hdfs/hdfs-site.xml
+wget https://raw.githubusercontent.com/jmargieh/hadoop_single_node_cluster/master/master-hdfs/hdfs-site.xml
 #OK
 rm -f yarn-site.xml
-wget https://raw.githubusercontent.com/jmargieh/hadoop_cluster/master/yarn-site.xml
+wget https://raw.githubusercontent.com/jmargieh/hadoop_single_node_cluster/master/yarn-site.xml
