9a02a70568d2046cdd0576dc70eb9bdd607e8da0
Jawad Margieh <J.margieh@gmail.com>
2016-04-29 14:44:17 +0300

    Single Node default hadoop configurations

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -27,12 +27,13 @@ sudo echo "export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_77" >> hadoop-env.sh
 cd /home/vagrant/
 
 #set home variables
-echo "export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_77" >> .bashrc
+echo 'export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_77' >> .bashrc
 echo 'export HADOOP_HOME=/home/vagrant/hadoop-2.7.2' >> .bashrc
 echo 'export HADOOP_MAPRED_HOME=$HADOOP_HOME' >> .bashrc
 echo 'export HADOOP_COMMON_HOME=$HADOOP_HOME' >> .bashrc
 echo 'export HADOOP_HDFS_HOME=$HADOOP_HOME' >> .bashrc
 echo 'export YARN_HOME=$HADOOP_HOME' >> .bashrc
+echo 'export HADOOP_CONF_DIR=/home/vagrant/hadoop-2.7.2/etc/hadoop' >> .bashrc
 echo 'export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native' >> .bashrc
 echo 'export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin' >> .bashrc
 echo 'export HADOOP_OPTS=-Djava.library.path=$HADOOP_HOME/lib' >> .bashrc
