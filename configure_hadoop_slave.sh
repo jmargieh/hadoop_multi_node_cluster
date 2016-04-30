42b282bd8ac526fa372a0bafc64ba4805447b51e
Jawad Margieh <J.margieh@gmail.com>
2016-04-30 10:27:15 +0300

    hadoop tmp dir

--- configure_hadoop_slave.sh
+++ configure_hadoop_slave.sh
@@ -5,6 +5,10 @@ sudo mkdir -p /usr/local/hadoop/hadoop_data/hdfs/namenode
 sudo mkdir -p /usr/local/hadoop/hadoop_data/hdfs/datanode
 sudo chmod -R 775 /usr/local/hadoop/
 
+sudo rm -rf /home/vagrant/hadoop-2.7.2/tmp/
+sudo mkdir -p /home/vagrant/hadoop-2.7.2/tmp/
+sudo chmod -R 775 /home/vagrant/hadoop-2.7.2/tmp/
+
 #sudo mkdir -p /home/vagrant/hadoop-2.7.2/hdfs/namenode
 #sudo chmod -R 775 /home/vagrant/hadoop-2.7.2/hdfs/namenode
 #sudo mkdir -p /home/vagrant/hadoop-2.7.2/hdfs/datanode
